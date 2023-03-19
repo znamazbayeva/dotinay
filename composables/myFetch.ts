ed25e07b630c5286432621af58fabbc1dbc2a728
Samatqyzy <zhuldyznamazbayeva@gmail.com>
2023-03-20 01:58:36 +0600

    feat(fetch): add pagination option to api

--- composables/myFetch.ts
+++ composables/myFetch.ts
@@ -1,9 +1,13 @@
-export const myFetch = async <T>(url : string) => {
+export const myFetch = async <T>( url : string, pagination = false) => {
   const result = ref<T| null> (null);
   const api = async() => {
     const res = await fetch(unref(`http://86.107.44.116/api/${url}/`));
     const data = await res.json();
-    result.value = data;
+    if (pagination) {
+      result.value = data.results
+    } else {
+      result.value = data;
+    }
   }
   await api()
   return {
