bcd0594e9e4a05a0a949948945ec345168102d41
Samatqyzy <zhuldyznamazbayeva@gmail.com>
2023-03-20 01:58:52 +0600

    feat(types): add post interface

--- types/index.ts
+++ types/index.ts
@@ -1,17 +1,26 @@
 export  interface Project {
-    id: number,
-    name?: string,
-    company?: string,
-    tools?: string,
-    description?: string,
-    url?: string,
-    photo?: string,
-    me?: number, 
-  }
-  export  interface Me {
-    id?: number,
-    name?: string,
-    occupation?: string,
-    intro?: string,
-    photo?: string,
-  }
+  id: number,
+  name?: string,
+  company?: string,
+  tools?: string,
+  description?: string,
+  url?: string,
+  photo?: string,
+  me?: number, 
+}
+export  interface Me {
+  id?: number,
+  name?: string,
+  occupation?: string,
+  intro?: string,
+  photo?: string,
+}
+
+export interface Post {
+  id?: number,
+  title?: string,
+  subtitle?: string,
+  content?: string,
+  photo?: string, 
+  me?: number,
+}
